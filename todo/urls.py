f531395bbab07bfa0c31e885a325726ababbe1d4
Anton Tsikhanovich <alluspeh@gmail.com>
2026-06-01 22:40:50 +0200

    DeleteView added to Task

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from todo.views import (TaskListView,
                         TaskCreateView,
-                        TaskUpdateView)
+                        TaskUpdateView,
+                        TaskDeleteView)
 
 app_name="todo"
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
-    path("tasks/create", TaskCreateView.as_view(), name="task-create"),path("tasks/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/create", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -7,6 +7,11 @@ from todo.models import Task
 class TaskListView(generic.ListView):
     model = Task
 
+    def get_context_data(self, **kwargs):
+        context = super(TaskListView, self).get_context_data(**kwargs)
+        context["next"] =self.request.META.get("HTTP_REFERER")
+        return context
+
 class TaskCreateView(generic.CreateView):
     model = Task
     fields = "__all__"
@@ -16,6 +21,11 @@ class TaskUpdateView(generic.UpdateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy('todo:task-list')
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy('task_list')
+    template_name = "todo/task_delete.html"
 
 
 

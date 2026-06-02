6b756497b5074857b462fa08b4263a1c07d7187d
Anton Tsikhanovich <alluspeh@gmail.com>
2026-06-02 21:25:11 +0200

    Form for tasks updated: added CheckBoxSelect i DateTimeInput

--- todo/forms.py
+++ todo/forms.py
@@ -1,12 +1,24 @@
 from django import forms
 
-from todo.models import Task
+from todo.models import Task, Tag
 
 
 class TaskForm(forms.ModelForm):
+    deadline = forms.DateTimeField(
+        required=False,
+        input_formats=["%Y-%m-%dT%H:%M"],
+        widget=forms.DateTimeInput(attrs={"class": "datepicker", "type": "datetime-local"})
+    )
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        required=False,
+        widget=forms.CheckboxSelectMultiple(),
+    )
     class Meta:
         model = Task
         fields = "__all__"
+
+
 
 class TagForm(forms.ModelForm):
     class Meta:
--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
+from todo.forms import TaskForm
 from todo.models import Task, Tag
 
 
@@ -11,12 +12,12 @@ class TaskListView(generic.ListView):
 
 class TaskCreateView(generic.CreateView):
     model = Task
-    fields = "__all__"
+    form_class = TaskForm
     success_url = reverse_lazy('todo:task-list')
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
-    fields = "__all__"
+    form_class = TaskForm
     success_url = reverse_lazy('todo:task-list')
 
 class TaskDeleteView(generic.DeleteView):
